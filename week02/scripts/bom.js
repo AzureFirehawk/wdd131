341662330f46226b3550321aad4e1b05230edf3f
AzureFirehawk <madison.thomas094@gmail.com>
2025-01-14 13:19:31 -0600

    Loops and BOM activities

---
+++ week02/scripts/bom.js
@@ -0,0 +1,25 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('ul');
+
+
+
+button.addEventListener('click', function (){
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+    if (input.value.trim() !== ''){
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        li.append(deleteButton);
+        list.append(li);
+        deleteButton.addEventListener('click', function() {
+            list.removeChild(li);
+            input.focus();
+        });
+        input.value = '';
+        input.focus();
+    } else {
+        input.value = '';
+        input.focus();
+    }
+});
