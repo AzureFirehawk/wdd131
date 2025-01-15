41e8cb323ba611a519ca32d64b80e4a3bbb9ae01
AzureFirehawk <madison.thomas094@gmail.com>
2025-01-15 17:42:58 -0600

    CSS update and error fix, wayfind added

--- scripts/temples.js
+++ scripts/temples.js
@@ -7,10 +7,13 @@ year.innerHTML = `${today.getFullYear()}`;
 lastmod.innerHTML = `Last Modification: ${document.lastModified}`;
 
 const hamButton = document.querySelector('#menu');
-const nav = document.querySelector('.nav');
+const navclass = document.querySelector('.nav');
+const nav = document.querySelector('nav');
+const h1 = document.querySelector('h1');
 
 hamButton.addEventListener('click', () => {
 	nav.classList.toggle('open');
 	hamButton.classList.toggle('open');
     h1.classList.toggle('open');
+    navclass.classList.toggle('open');
 });
